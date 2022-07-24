8b7cb4aab4e9ae4db5b9da8e33963f8ba5c12174
GamerSubhan <74736141+GamerSubhan@users.noreply.github.com>
2022-07-24 06:48:34 -0700

    1

---
+++ Assets/BoughtAssets/Spells/Spells Pack/Demo/Scripts/Projectile.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public GameObject ExplosionPrefab;
+    public float DestroyExplosion = 4.0f;
+    public float DestroyChildren = 2.0f;
+    public float lifeTime = 0.5f;
+    public Vector2 Velocity;
+    public float speed = 10f;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * speed);
+    }
+
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            gameObject.SetActive(false);
+            lifeTime = 0.5f;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.name);
+        ApplyDamageOverTime(col);
+        DisplayHiteffect();
+        DestroyGameObject();
+    }
+
+    private static void ApplyDamageOverTime(Collision col)
+    {
+        var statusEffectManager = col.gameObject.GetComponent<StatusEffectManager>();
+        if (statusEffectManager != null)
+        {
+            statusEffectManager.ApplyBurn(4);
+        }
+    }
+
+    private void DisplayHiteffect()
+    {
+        var exp = Instantiate(ExplosionPrefab, transform.position, ExplosionPrefab.transform.rotation);
+        Destroy(exp, DestroyExplosion);
+    }
+
+    private void DestroyGameObject()
+    {
+        gameObject.SetActive(false);
+
+        //Transform child = transform.GetChild(0);
+        //transform.DetachChildren();
+        //Destroy(child.gameObject, DestroyChildren);
+        //Destroy(gameObject);
+    }
+}
